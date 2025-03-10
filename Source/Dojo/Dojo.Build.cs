708a96ba2256e301be418c82d36482f03c3e39b3
caillef <corentin.cailleaud@caillef.com>
2025-03-10 22:23:37 +0100

    update 1.2.2

--- Source/Dojo/Dojo.Build.cs
+++ Source/Dojo/Dojo.Build.cs
@@ -5,15 +5,13 @@ using System.IO;
 
 public class Dojo : ModuleRules
 {
-    protected readonly string Version = "1.0.1";
+    protected readonly string Version = "1.2.2";
     protected string VersionPath { get => Path.Combine(ModuleDirectory, Version); }
     protected string LibraryPath { get => Path.Combine(VersionPath, "lib"); }
 
     public Dojo(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-        
-        //Type = ModuleType.External;
 
         PublicSystemIncludePaths.Add(Path.Combine(VersionPath, "include"));
 
@@ -21,7 +19,6 @@ public class Dojo : ModuleRules
             new string[]
             {
                 "Core",
-                // ... other dependencies ...
             }
         );
 
@@ -30,7 +27,6 @@ public class Dojo : ModuleRules
             {
                 "CoreUObject",
                 "Engine",
-                // ... other dependencies ...
             }
         );
 
@@ -38,10 +34,10 @@ public class Dojo : ModuleRules
         {
             string DylibPath = Path.Combine(LibraryPath, "Mac", "libdojo_c.dylib");
             PublicAdditionalLibraries.Add(DylibPath);
-            
+
             // Add runtime dependency for Mac
             RuntimeDependencies.Add("$(PluginDir)/Source/Dojo/" + Version + "/lib/Mac/libdojo_c.dylib");
-            
+
             // Delay load the dylib
             PublicDelayLoadDLLs.Add("libdojo_c.dylib");
         }
@@ -49,23 +45,6 @@ public class Dojo : ModuleRules
         {
             // Use iOS library
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, Version, "lib", "IOS", "libdojo_c.a"));
-
-            // Add runtime dependency for Mac
-            //RuntimeDependencies.Add("$(PluginDir)/Source/Dojo/" + Version + "/lib/iOS/libdojo_c.a");
-            
-            // Delay load the dylib
-            //PublicDelayLoadDLLs.Add("libdojo_c.a");
-            
-            
-            //PublicAdditionalFrameworks.Add(
-            //                new Framework(
-            //                    "Dojo",                                                        // Framework name
-            //                    "../../ThirdPartyFrameworks/Dojo.embeddedframework.zip"       // Zip name
-            //
-                    //null, true                              // Resources we need copied and staged
-            //                )
-            //);
-            
         }
     }
 }
