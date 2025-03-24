6487bb5a97bb8d2959a681bbe7c8333bf40bc2b3
caillef <corentin.cailleaud@caillef.com>
2025-03-24 15:19:19 +0100

    feat: added utils functions

--- Source/Dojo/Public/DojoModule.h
+++ Source/Dojo/Public/DojoModule.h
@@ -24,34 +24,40 @@ class DOJO_API FDojoModule : public IModuleInterface
 public:
     virtual void StartupModule() override;
     virtual void ShutdownModule() override;
-    
+
     static ToriiClient *CreateToriiClient(const char *torii_url, const char *world_str);
-    
+
     static ResultCArrayEntity GetEntities(ToriiClient *client, const char *query);
-    
+
     static struct ResultSubscription OnEntityUpdate(ToriiClient *client, const char *query_str, void *user_data, EntityUpdateCallback callback);
-    
+
     static void ExecuteRaw(Account *account, const char *to, const char *selector, const std::vector<std::string> &feltsStr);
 
     static void ExecuteFromOutside(ControllerAccount *account, const char *to, const char *selector, const std::vector<std::string> &feltsStr);
 
     static Account *CreateAccount(const char *rpc_url, const char *address, const char *private_key);
-    
+
     static Account *CreateBurner(const char *rpc_url, Account *master_account);
-    
+
     static FString AccountAddress(Account *account);
-    
+
+    static FString ControllerAccountAddress(ControllerAccount *account);
+
     static void ControllerGetAccountOrConnect(const char* rpc_url, const char* chain_id, const struct Policy *policies, size_t nb_policies, ControllerAccountCallback callback);
 
     static void SubscriptionCancel(struct Subscription *subscription);
-    
+
     static void AccountFree(struct Account *account);
-    
+
     static void EntityFree(struct Entity *entity);
-    
+
     static void ModelFree(struct Struct *model);
 
     static void TyFree(struct Ty *ty);
 
     static void CArrayFree(void *data, int len);
+
+    static FString bytes_to_fstring(const uint8_t* data, size_t length, bool addPrefix = true);
+
+    static void string_to_bytes(const std::string& hex_str, uint8_t* out_bytes, size_t max_bytes);
 };
